4e5525245e36d7f7569acf5c65fb330cfa778568
Micranthum <ramoncito.vl@gmail.com>
2016-12-10 21:46:40 -0600

    Add

---
+++ Laboratorio_2/E4.py
@@ -0,0 +1,8 @@
+print "Este programa te da las coordenadas de un poligono regular con el numero de lados que usted elija"
+a=input(" Por favor ingrese el numero de lados de su poligono regular: ")
+lista=[]
+for i in range(a):
+    x1=[0,i]
+    y1=[i,0]
+    lista.append(x1+y1)
+print lista
